a6b7c553d22df3d437439b9f0f286cae5ae6efc5
胡俊豪 <jimhu@digitalgd.com.cn>
2021-07-28 11:36:53 +0800

    init

---
+++ src/main/java/com/xbleey/TestApplication.java
@@ -0,0 +1,19 @@
+package com.xbleey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author jimhu
+ */
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
